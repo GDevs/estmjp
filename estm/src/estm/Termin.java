2376e7cec94b1b2c3145505b3659dd4e629e2f3b
Adrian C. Hinrichs <adrian-corin.hinrichs@gmx.de>
2015-02-24 13:03:18 +0100

    + Termin.java created

---
+++ estm/src/estm/Termin.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package estm;
+
+/**
+ * @author adrhin130898
+ *
+ */
+package estm;
+
+public class Termin {
+
+	private int zeitschiene;
+	private Elter elter;
+	private Lehrer lehrer;
+		
+	public Termin(int zeitschiene,Elter elter, Lehrer lehrer){
+		this.zeitschiene = zeitschiene;
+		this.elter = elter;
+		this.lehrer = lehrer; 
+	}
+		
+	public int getZeitschiene(){
+		return zeitschiene
+	}
+	
+	public Elter getElter(){
+		return elter;
+	}
+	
+	public Lehrer getLehrer(){
+		return lehrer;
+	}
+}
