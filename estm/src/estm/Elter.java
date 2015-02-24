4ee2690bf21d124f7dd1fcd1c3421d3bf0a5ecdc
Georg Dorndorf <georg.dorndorf@outlook.com>
2015-02-24 12:41:30 +0100

    Elter.java created

--- estm/src/estm/Elter.java
+++ estm/src/estm/Elter.java
@@ -1,5 +1,17 @@
 package estm;
 
 public class Elter {
-
+	private String name;
+	private int ID;
+	
+	
+	
+	public Elter(String name, int ID){
+		this.name = name;
+		this.ID = ID;
+	}
+	
+	public String getName(){ return name;}
+	
+	public int getID(){return ID;}
 }
