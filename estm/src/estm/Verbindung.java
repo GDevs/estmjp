42c910d281ac19814d2cdcf987d695d805f20624
benmue220697 <benmue220697@A116-PC05.GY-SLG-AC.mnsplus>
2015-02-27 09:07:47 +0100

    + Verbindung.java created (jetzt wirklich)

---
+++ estm/src/estm/Verbindung.java
@@ -0,0 +1,49 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Verbindung
+{
+    java.sql.Connection conn;
+    
+    public Verbindung()
+    {
+         try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            System.out.println("Fehler beim Erzeugen einer neuen Instanz!");
+        }
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://meltzow.homelinux.net/leo", "leo", "leo");
+        } catch (SQLException ex) {
+            System.out.println("Fehler beim Aufbau der Verbindung!");
+        }
+    }
+    
+    
+    public String query(String pSelect)
+    {
+        String temp="";
+      //    System.out.println("SQL Befehl:"+pSelect);
+        try{
+            Statement stmt = conn.createStatement();
+            stmt.execute(pSelect);
+            ResultSet rs = stmt.getResultSet();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int spalten = rsmd.getColumnCount();
+            while (rs.next()){
+                for (int i = 1; i <= spalten; i++){
+                    temp=temp+rs.getString(i);
+                                    }
+            }
+            rs.close();
+            stmt.close();            
+        } catch (SQLException ex) {
+            System.out.println("Fehler beim Statement!");
+        }
+     //   System.out.println("SQL Ergebnis: "+temp);
+        return temp;
+    }
+}
