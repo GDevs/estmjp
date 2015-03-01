0eb67ac6a5acfc5c672d0a78c9148b00f2a7dc1f
ACHinrichs <adrian-corin.hinrichs@gmx.de>
2015-03-01 14:58:44 +0100

    + eltern und lehrer zu personen zusammengeführt (List in ESTM)

--- estm/src/estm/ESTM.java
+++ estm/src/estm/ESTM.java
@@ -22,11 +22,10 @@ import java.util.List;
  * 
  */
 public class ESTM {
-	//private Verbindung verbindung;
+	// private Verbindung verbindung;
 	private Connection conn;
 
-	private List<Person> eltern;
-	private List<Person> lehrer;
+	private List<Person> personen;
 	private List<Termin> wuensche;
 
 	private Person user;
@@ -35,9 +34,10 @@ public class ESTM {
 	 * 
 	 */
 	public ESTM(Connection connection, Person user) {
-		//this.verbindung = verbindung;
+		// this.verbindung = verbindung;
 		this.user = user;
 		this.conn = connection;
+		personen = new ArrayList<Person>();
 	}
 
 	/**
@@ -125,77 +125,70 @@ public class ESTM {
 		try {
 			rsmd = rs.getMetaData();
 			if (rsmd.getTableName(0).equals("personen")) {
-				eltern.clear();
-				lehrer.clear();
-				Person person = new Person(rs.getInt("ID"), rs.getString("name"),
-						rs.getString("vorname"), rs.getInt("rechte"),
-						rs.getString("status"), rs.getString("kennwort"));
-				if (person.getStatus().equals("L")) {
-					lehrer.add(person);
-				} else if (person.getStatus().equals("E")) {
-					eltern.add(person);
+				personen.clear();
+				if (rs.first()) {
+					do {
+						Person person = new Person(rs.getInt("ID"),
+								rs.getString("name"), rs.getString("vorname"),
+								rs.getInt("rechte"), rs.getString("status"),
+								rs.getString("kennwort"));
+						personen.add(person);
+					} while (rs.next());
 				}
 			} else if (rsmd.getTableName(0).equals("termine")) {
-				Person pElter = null;
-				int person1ID = rs.getInt("person1");
-				Person pLehrer = null;
-				int person2ID = rs.getInt("person2");
-				for (Person p : eltern) {
-					if (p.getID() == person1ID || p.getID() == person2ID) {
-						if (p.getStatus().equals("E")) {
-							pElter = p;
-						} else {
-							pLehrer = p;
+				if (rs.first()) {
+					do {
+						Person pElter = null;
+						int person1ID = rs.getInt("person1");
+						Person pLehrer = null;
+						int person2ID = rs.getInt("person2");
+						for (Person p : personen) {
+							if (p.getID() == person1ID
+									|| p.getID() == person2ID) {
+								if (p.getStatus().equals("E")) {
+									pElter = p;
+								} else {
+									pLehrer = p;
+								}
+							}
 						}
-					}
+						if (pElter == null || pLehrer == null) {
+							return false;
+						}// Wenn eine der Personen nicht gefunden werden konnte,
+							// wird
+							// abgebrochen
+						Termin termin = new Termin(rs.getInt("zeitpunkt"),
+								pElter, pLehrer);
+						pElter.addTermin(termin);
+						pLehrer.addTermin(termin);
+					} while (rs.next());
 				}
-				for (Person p : lehrer) {
-					if (p.getID() == person1ID || p.getID() == person2ID) {
-						if (p.getStatus().equals("E")) {
-							pElter = p;
-						} else {
-							pLehrer = p;
-						}
-					}
-				}
-				if (pElter == null || pLehrer == null) {
-					return false;
-				}// Wenn eine der Personen nicht gefunden werden konnte, wird
-					// abgebrochen
-				Termin termin = new Termin(rs.getInt("zeitpunkt"), pElter,
-						pLehrer);
-				pElter.addTermin(termin);
-				pLehrer.addTermin(termin);
 			} else if (rsmd.getTableName(0).equals("terminwunsch")) {
-				Person pElter = null;
-				int person1ID = rs.getInt("person1");
-				Person pLehrer = null;
-				int person2ID = rs.getInt("person2");
-				for (Person p : eltern) {
-					if (p.getID() == person1ID || p.getID() == person2ID) {
-						if (p.getStatus().equals("E")) {
-							pElter = p;
-						} else {
-							pLehrer = p;
+				if (rs.first()) {
+					Person pElter = null;
+					int person1ID = rs.getInt("person1");
+					Person pLehrer = null;
+					int person2ID = rs.getInt("person2");
+					for (Person p : personen) {
+						if (p.getID() == person1ID || p.getID() == person2ID) {
+							if (p.getStatus().equals("E")) {
+								pElter = p;
+							} else {
+								pLehrer = p;
+							}
 						}
 					}
+					if (pElter == null || pLehrer == null) {
+						return false;
+					}// Wenn eine der Personen nicht gefunden werden konnte,
+						// wird
+						// abgebrochen
+					Termin termin = new Termin(rs.getInt("zeitpunkt"), pElter,
+							pLehrer);
+					wuensche.add(termin);
 				}
-				for (Person p : lehrer) {
-					if (p.getID() == person1ID || p.getID() == person2ID) {
-						if (p.getStatus().equals("E")) {
-							pElter = p;
-						} else {
-							pLehrer = p;
-						}
-					}
-				}
-				if (pElter == null || pLehrer == null) {
-					return false;
-				}// Wenn eine der Personen nicht gefunden werden konnte, wird
-					// abgebrochen
-				Termin termin = new Termin(rs.getInt("zeitpunkt"), pElter,
-						pLehrer);
-				wuensche.add(termin);
+				while (rs.next())
+					;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -204,40 +197,49 @@ public class ESTM {
 		}
 		return true;
 	}
-	
+
 	/**
-	 * Übersetzt die ausgabe Datei des LP solvers in die relationale Datenbankstruktur
+	 * Übersetzt die ausgabe Datei des LP solvers in die relationale
+	 * Datenbankstruktur
 	 */
-	public void parseLpSol(int version){
+	public void parseLpSol(int version) {
 		try {
 			Statement stmt = conn.createStatement();
 			stmt.execute("DELETE FROM `termine` WHERE `version`=" + version);
-			
-			List<String> termine = new ArrayList<String>(); 
+
+			List<String> termine = new ArrayList<String>();
 			BufferedReader br;
 			br = new BufferedReader(new FileReader(
 					"F:/source/estmjp/estm/src/estm/modell/solution.dat"));
-			
+
 			String sCurrentLine;
 			while ((sCurrentLine = br.readLine()) != null) {
 				termine.add(sCurrentLine);
 			}
-			
-			for (String termin : termine){
+
+			for (String termin : termine) {
 				String t[];
-				//System.out.println(termin);
+				// System.out.println(termin);
 				t = termin.split("\\$|#| ");
-				System.out.println(" Termin: " + t[1] + " Lehrer: " +  t[2] + " Elter: " + t[3]);
-				if(t[0].equals("x")){
+				System.out.println(" Termin: " + t[1] + " Lehrer: " + t[2]
+						+ " Elter: " + t[3]);
+				if (t[0].equals("x")) {
 					String q = "INSERT INTO termine(person1, person2, zeitpunkt, version) "
-							+ "VALUES(" + t[2] + "," + t[3] + "," + t[1] + "," + version + ");";
+							+ "VALUES("
+							+ t[2]
+							+ ","
+							+ t[3]
+							+ ","
+							+ t[1]
+							+ ","
+							+ version + ");";
 					System.out.println(q);
 					System.out.println("sql query erfolgreich");
-					//Statement stmt = conn.createStatement();
+					// Statement stmt = conn.createStatement();
 					stmt.execute(q);
 				}
 			}
-			
+
 			br.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -245,90 +247,105 @@ public class ESTM {
 		}
 
 	}
-	
+
 	/**
 	 * @param pPerson
 	 */
-	public boolean insertPersonIntoDatabase(Person person){
-		try{
-		Statement stmt = conn.createStatement();
-		String request = "INSERT INTO personen(`ID`, `name`, vorname, rechte, `status`, kennwort) "
-							+ "VALUES(" + person.getID() + ",\"" + person.getName() + "\",\"" + person.getVorname() + "\"," + person.getRechte() + ",\"" + 
-							person.getStatus() + "\",\"" + person.getPassword() +"\");";
-		stmt.execute(request);
-		System.out.println(request);
-		return true;
-		}
-		catch(Exception e){
+	public boolean insertPersonIntoDatabase(Person person) {
+		try {
+			Statement stmt = conn.createStatement();
+			String request = "INSERT INTO personen(`ID`, `name`, vorname, rechte, `status`, kennwort) "
+					+ "VALUES("
+					+ person.getID()
+					+ ",\""
+					+ person.getName()
+					+ "\",\""
+					+ person.getVorname()
+					+ "\","
+					+ person.getRechte()
+					+ ",\""
+					+ person.getStatus()
+					+ "\",\""
+					+ person.getPassword() + "\");";
+			stmt.execute(request);
+			System.out.println(request);
+			return true;
+		} catch (Exception e) {
 			e.printStackTrace(); // boolean fürs error handling usw.
 			return false;
 		}
 	}
-	
+
 	/**
 	 * @param pTermin
 	 */
-	public void insertTerminwunschIntoDatabase(Termin pTermin){
-		try{
-		Statement stmt = conn.createStatement();
-		String request = "INSERT INTO terminwunsch(Person1, Person2, Zeitschiene) "
-				         + "VALUES("+pTermin.getLehrer()+","+pTermin.getElter()+","+pTermin.getZeitschiene()+ ");";
-		stmt.execute(request);
-		}
-		catch(Exception e){
+	public void insertTerminwunschIntoDatabase(Termin pTermin) {
+		try {
+			Statement stmt = conn.createStatement();
+			String request = "INSERT INTO terminwunsch(Person1, Person2, Zeitschiene) "
+					+ "VALUES("
+					+ pTermin.getLehrer()
+					+ ","
+					+ pTermin.getElter()
+					+ ","
+					+ pTermin.getZeitschiene()
+					+ ");";
+			stmt.execute(request);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Liest Benutzer aus einer csv Datei ein
-	 * @param path Pfad zur csv Datei
+	 * 
+	 * @param path
+	 *            Pfad zur csv Datei
 	 */
-	public boolean addUsrFromFile(String path){
+	public boolean addUsrFromFile(String path) {
 		try {
-			List<String> personen = new ArrayList<String>(); 
+			List<String> personen = new ArrayList<String>();
 			BufferedReader br;
 			br = new BufferedReader(new FileReader(path));
-			
+
 			String sCurrentLine;
 			while ((sCurrentLine = br.readLine()) != null) {
 				personen.add(sCurrentLine);
 			}
-			//Statement stmt = conn.createStatement();
-			
-			for (String person : personen){
+			// Statement stmt = conn.createStatement();
+
+			for (String person : personen) {
 				String t[];
 				t = person.split(";| ");
-				if(!t[0].equals("#")){
-					Person p = new Person(Integer.parseInt(t[0]), t[1], t[2], Integer.parseInt(t[3]), t[4], t[5]);
+				if (!t[0].equals("#")) {
+					Person p = new Person(Integer.parseInt(t[0]), t[1], t[2],
+							Integer.parseInt(t[3]), t[4], t[5]);
 					insertPersonIntoDatabase(p);
-					//String q = "INSERT INTO personen(`ID`, `name`, vorname, rechte, `status`, kennwort) "
-						//	+ "VALUES(" + t[0] + ",\"" + t[1] + "\",\"" + t[2] + "\"," + t[3] + ",\"" + 
-							//t[4] + "\",\"" + t[5] +"\");";
-					//System.out.println(q);
-					//System.out.println("sql query erfolgreich");		
-					//stmt.execute(q);
+					// String q =
+					// "INSERT INTO personen(`ID`, `name`, vorname, rechte, `status`, kennwort) "
+					// + "VALUES(" + t[0] + ",\"" + t[1] + "\",\"" + t[2] +
+					// "\"," + t[3] + ",\"" +
+					// t[4] + "\",\"" + t[5] +"\");";
+					// System.out.println(q);
+					// System.out.println("sql query erfolgreich");
+					// stmt.execute(q);
 				}
 			}
 			br.close();
 			return true;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			return false;
 		}
 	}
-	
-	/*private static void writeUsrsToDat(){
-		try {
-			PrintWriter writer = new PrintWriter("F:/source/estmjp/estm/src/estm/usrs.csv", "ASCII");
-			for (Person i : personen){
-				writer.println(i.getID() + ";" + i.getName() + ";" + i.getVorname() + ";" + i.getRechte() + ";" + i.getStatus() + ";" + i.getPassword());
-			}
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
-		System.out.println("DONE");
-	}*/
+
+	/*
+	 * private static void writeUsrsToDat(){ try { PrintWriter writer = new
+	 * PrintWriter("F:/source/estmjp/estm/src/estm/usrs.csv", "ASCII"); for
+	 * (Person i : personen){ writer.println(i.getID() + ";" + i.getName() + ";"
+	 * + i.getVorname() + ";" + i.getRechte() + ";" + i.getStatus() + ";" +
+	 * i.getPassword()); } writer.close(); } catch (Exception e) {
+	 * e.printStackTrace(); } System.out.println("DONE"); }
+	 */
 }
